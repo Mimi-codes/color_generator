5e389f49c5a57eaa1b369ad18f494209bfd30886
Mimi-codes <65224611+Mimi-codes@users.noreply.github.com>
2021-12-18 22:38:06 +0100

    Add files via upload

---
+++ precache-manifest.0fdacf701b16b30d98780c947013b709.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "726f7581c1dd8123fcdd4e789ebe795c",
+    "url": "/index.html"
+  },
+  {
+    "revision": "f4d1f2908d84dd6a04b9",
+    "url": "/static/css/main.f6e8d1ab.chunk.css"
+  },
+  {
+    "revision": "cf6bca8e816ecf72b2d4",
+    "url": "/static/js/2.11f691c1.chunk.js"
+  },
+  {
+    "revision": "1dbab337b90e8f2eb0a4a79d2378f49b",
+    "url": "/static/js/2.11f691c1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f4d1f2908d84dd6a04b9",
+    "url": "/static/js/main.0fde2ead.chunk.js"
+  },
+  {
+    "revision": "7402b3a51c6a0325ccf5",
+    "url": "/static/js/runtime-main.297f6f71.js"
+  }
+]);
